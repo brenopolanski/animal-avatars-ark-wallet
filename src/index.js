15b40e8723f81a0bc5cb85c6752f82f8caffe8b4
brenopolanski <breno.polanski@gmail.com>
2020-01-19 22:45:46 -0300

    Add more animals

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ module.exports = {
 
   getComponentPaths() {
     return {
-      'chick': 'components/avatars/chick.js'
+      'chick': 'components/avatars/chick.js',
+      'chicken': 'components/avatars/chicken.js',
+      'narwhal': 'components/avatars/narwhal.js',
+      'parrot': 'components/avatars/parrot.js',
+      'penguin': 'components/avatars/penguin.js',
+      'snake': 'components/avatars/snake.js'
     };
   },
 
   getAvatars() {
     return [
-      'chick'
+      'chick',
+      'chicken',
+      'narwhal',
+      'parrot',
+      'penguin',
+      'snake'
     ];
   }
 };
